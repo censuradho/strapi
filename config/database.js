a6534792ec3371554766c5071464845524928c0d
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-06 10:56:21 -0300

    feat(product)

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('POSTGRES_DB', 'strapi'),
+      user: env('POSTGRES_USER', 'strapi'),
+      password: env('POSTGRES_PASSWORD', 'password'),
       ssl: env.bool(true),
     },
   },
