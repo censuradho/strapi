d411dc526c4544d82ea64d8a5f265bedf640daa9
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-08 13:53:10 -0300

    reafactor(config): database

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      user: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_USER', 'strapi') : env('PGUSER', 'strapi'),
-      database: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_DB', 'strapi') : env('PGDATABASE', 'strapi'),
-      password: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_PASSWORD', 'strapi') : env('PGPASSWORD', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      database: env('PGDATABASE', 'strapi'),
+      password: env('PGPASSWORD', 'strapi'),
       ssl: env.bool(true),
     },
   },
