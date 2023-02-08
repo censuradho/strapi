f891e7d0ee7a3985198940bce2a597b276b9fb27
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-08 11:53:50 -0300

    refactor(config): database

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('POSTGRES_DB', 'strapi'),
-      user: env('POSTGRES_USER', 'strapi'),
-      password: env('POSTGRES_PASSWORD', 'password'),
+      user: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_USER', 'strapi') : env('PGUSER', 'strapi'),
+      database: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_DB', 'strapi') : env('PGDATABASE', 'strapi'),
+      password: env('NODE_ENV', 'develop') === 'develop' ? env('POSTGRES_PASSWORD', 'strapi') : env('PGPASSWORD', 'strapi'),
       ssl: env.bool(true),
     },
   },
